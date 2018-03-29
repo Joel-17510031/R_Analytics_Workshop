3cf4c787063054537c90b64975fee94593f6052c
Joel-17510031 <35612321+Joel-17510031@users.noreply.github.com>
2018-03-29 15:52:54 +0530

    1

---
+++ data_structures.R
@@ -0,0 +1,16 @@
+#Vectors----
+
+v1<- c("A","B","C")
+v1
+class(v1)
+v2<- c(1,2,3)
+class(v2)
+v3<- c(TRUE, FALSE, TRUE)
+class(v3)
+(v4<- 100:200)
+class(v4)
+v5=seq(1,10,2)
+class(v5)
+v4[c(1,3,4)]
+v7<- c("A", "C")
+v1[v1 %in% v7]
