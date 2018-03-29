07c58937e4e151a9df2ad3998fcef551a77d4bf5
Joel-17510031 <35612321+Joel-17510031@users.noreply.github.com>
2018-03-29 15:53:17 +0530

    1

--- data_structures.R
+++ data_structures.R
@@ -14,3 +14,21 @@ class(v5)
 v4[c(1,3,4)]
 v7<- c("A", "C")
 v1[v1 %in% v7]
+#v1["A"] position not specified
+v1[1]
+
+
+v8<- 1:5
+names(v8)<- c("anish","shlok", "jaimin", "bhavya", "nihali")
+v8["anish"]
+v8[2:4]
+
+#datafream ----
+df=data.frame(rollno=c(1,2,3),name=c("Rohit","Lalit","Hitesh"),course=c("MBA","BBA","MCA"),dept=c("Dept1","Dept1","Dept2"),marks=floor(runif(3,50,100)))
+df
+df=fix(df)
+df
+str(df)
+class(df)
+summary(df)
+
