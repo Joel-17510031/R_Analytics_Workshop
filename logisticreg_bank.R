faaa0142870e540444491c75c6fac74b7ff4db84
Joel-17510031 <35612321+Joel-17510031@users.noreply.github.com>
2018-03-31 13:07:20 +0530

    2

--- logisticreg_bank.R
+++ logisticreg_bank.R
@@ -4,4 +4,20 @@ head(Default)
 dim(Default)
 summary(Default)
 logit<- glm(default ~ income + balance +student, family="binomial", data=Default)
-logit
+summary(logit)
+
+library(dplyr)
+# genertaing testing data
+ndata<- (slice(Default,seq(0,n(),500)))
+ndata
+
+# seing the percentage of student 
+prop.table(table(Default$default,Default$student))
+addmargins(prop.table(table(Default$default,Default$student)))
+
+# seeing the propability of prediction on testing data
+predicted_values<- predict(logit, newdata=ndata, type="response")
+# column binding predicted values to original data
+cbind(ndata,predicted_values)
+# Add the predict as Yes/No based on prediced values
+ndata %>% mutate(predict=ifelse(predicted_values<0.5,"No","Yes"))
